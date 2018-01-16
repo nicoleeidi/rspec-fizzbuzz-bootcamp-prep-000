9f59a3262ae14bbbf76d5b9822548cafa95cf52a
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-16 16:32:40 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(x)
+  if x%3 == 0 && x%5 == 0
+    return "FizzBuzz"
+  end
+  elsif x%3 == 0 
+    return "Fizz"
+  end 
+  elsif x%5== 0
+    return "Buzz"
+  end
+  else
+    return nil
+  end
+end 
