0fac10892a2cbaa1594383f4c16ffcc99c4039af
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-16 16:47:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(x)
-  if x%3 == 0 && x%5 == 0
+  if x % 3 == 0 && x % 5 == 0
     return "FizzBuzz"
   end
-  elsif x%3 == 0
+  elsif x % 3 == 0
     return "Fizz"
   end
-  elsif x%5== 0
+  elsif x % 5== 0
     return "Buzz"
   end
   else
